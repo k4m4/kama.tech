663914907e0df1d163bd69b05fb17f9096cf0f0f
k4m4 <nikolaskam@gmail.com>
2020-10-23 21:06:38 +0300

    Add opacity transition to card component

--- components/molecules/card.js
+++ components/molecules/card.js
@@ -2,7 +2,7 @@ import { string } from 'prop-types';
 
 const Card = ({ title, subtitle, iconPath }) => {
 	return (
-		<div className="relative border border-gray-900 bg-black px-10 lg:px-16 mb-10 lg:mb-0 block h-74 lg:h-48 w-full hover:border-white focus:border-white focus:outline-none">
+		<div className="relative border border-gray-900 bg-black px-10 lg:px-16 mb-10 lg:mb-0 block h-74 lg:h-48 w-full hover:border-white focus:border-white focus:outline-none transition-all ease-in duration-200">
 			<div className="flex items-start lg:items-center justify-around lg:justify-between flex-col lg:flex-row h-full w-full py-10 lg:py-0">
 				<div className="mt-16 lg:mt-0 order-2 lg:order-none w-full">
 					<h2 className="mb-6 font-bold font-mono text-xl">
