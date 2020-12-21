fbefc52a0486f65a00f2ce740efd51d95a65fecb
k4m4 <nikolaskam@gmail.com>
2020-12-21 22:59:37 +0200

    Remove redundant Meie Script font

--- components/atoms/fonts.tsx
+++ components/atoms/fonts.tsx
@@ -1,15 +1,9 @@
 const Fonts = () => {
 	return (
-		<>
-			<link
-				href="https://fonts.googleapis.com/css?family=Space+Mono&display=swap"
-				rel="stylesheet"
-			/>
-			<link
-				href="https://fonts.googleapis.com/css2?family=Meie+Script&display=swap"
-				rel="stylesheet"
-			/>
-		</>
+		<link
+			href="https://fonts.googleapis.com/css?family=Space+Mono&display=swap"
+			rel="stylesheet"
+		/>
 	);
 };
 
